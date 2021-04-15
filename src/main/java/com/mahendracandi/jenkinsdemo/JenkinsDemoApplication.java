4c65762f478bc759019c0696d9e6a3dbe4fa9490
MahendraCandi <abdumaadjidcandi@gmail.com>
2021-04-15 14:23:35 +0700

    update to war

--- src/main/java/com/mahendracandi/jenkinsdemo/JenkinsDemoApplication.java
+++ src/main/java/com/mahendracandi/jenkinsdemo/JenkinsDemoApplication.java
@@ -2,9 +2,10 @@ package com.mahendracandi.jenkinsdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JenkinsDemoApplication {
+public class JenkinsDemoApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JenkinsDemoApplication.class, args);
