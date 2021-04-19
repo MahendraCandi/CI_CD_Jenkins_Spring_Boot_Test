a130976c74d15d958f42c6ad15a8ff2462dfcfb7
MahendraCandi <abdumaadjidcandi@gmail.com>
2021-04-19 15:20:05 +0700

    add simple test

--- src/test/java/com/mahendracandi/jenkinsdemo/JenkinsDemoApplicationTests.java
+++ src/test/java/com/mahendracandi/jenkinsdemo/JenkinsDemoApplicationTests.java
@@ -3,11 +3,46 @@ package com.mahendracandi.jenkinsdemo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+//@SpringBootTest
 class JenkinsDemoApplicationTests {
 
+	Calculator underTest = new Calculator();
+
 	@Test
-	void contextLoads() {
+	void itShouldAddTwoNumbers() {
+		// given
+		int first = 20;
+		int second = 30;
+
+		// when
+		int result = underTest.add(first, second);
+
+		// then
+		assertThat(result).isEqualTo(50);
+	}
+
+	@Test
+	void itShouldZero() {
+		// given
+		int first = 10;
+		int second = 10;
+
+		// when
+		int result = underTest.subtract(first, second);
+
+		// then
+		assertThat(result).isEqualTo(0);
+	}
+
+	class Calculator {
+		int add(int a, int b) {
+			return a + b;
+		}
+		int subtract(int a, int b) {
+			return a - b;
+		}
 	}
 
 }
